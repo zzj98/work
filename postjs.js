d19760f173465c89b7c0462ac89a082b04eb0cc8
zzj98 <1157050696@qq.com>
2019-08-04 10:41:08 +0800

    work

--- postjs.js
+++ postjs.js
@@ -1,81 +1,3 @@
-/*
-//1.导入http模块
-var http = require('http');
-//导入文件模块
-var fs = require('fs');
-//导入路径模块
-var path = require('path');
-//导入querystring模块（解析post请求数据）
-var querystring = require('querystring');
-
-//2.创建服务器
-function handle_request(req,res){
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-}
-var app = http.createServer(handle_request);
-
-//3.添加响应事件
-app.on('request', function (req, res) {
-
-    console.log(req.method);
-
-    //1.通过判断url路径和请求方式来判断是否是表单提交
-    if (req.url === '/heroAdd' && req.method === 'POST') {
-        /!**服务端接收post请求参数的流程
-         * （1）给req请求注册接收数据data事件（该方法会执行多次，需要我们手动累加二进制数据）
-         *      * 如果表单数据量越多，则发送的次数越多，如果比较少，可能一次就发过来了
-         *      * 所以接收表单数据的时候，需要通过监听 req 对象的 data 事件来取数据
-         *      * 也就是说，每当收到一段表单提交过来的数据，req 的 data 事件就会被触发一次，同时通过回调函数可以拿到该 段 的数据
-         * （2）给req请求注册完成接收数据end事件（所有数据接收完成会执行一次该方法）
-         *!/
-        //创建空字符叠加数据片段
-        var data = '';
-
-        //2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
-        req.on('data', function (chunk) {
-            // chunk 默认是一个二进制数据，和 data 拼接会自动 toString
-            data += chunk;
-        });
-
-        // 3.当接收表单提交的数据完毕之后，就可以进一步处理了
-        //注册end事件，所有数据接收完成会执行一次该方法
-        req.on('end', function () {
-
-            //（1）.对url进行解码（url会对中文进行编码）
-            data = decodeURI(data);
-            console.log(data);
-
-            /!**post请求参数不能使用url模块解析，因为他不是一个url，而是一个请求体对象 *!/
-
-                //（2）.使用querystring对url进行反序列化（解析url将&和=拆分成键值对），得到一个对象
-                //querystring是nodejs内置的一个专用于处理url的模块，API只有四个，详情见nodejs官方文档
-            var dataObject = querystring.parse(data);
-            console.log(dataObject);
-        });
-    }else if (req.url === '/index' && req.method === 'POST') {
-        fs.readFile('./index/post.html', function (err, data) {
-            if (err) {
-                throw err;
-            }
-            res.end(data);
-        });
-    } else if (req.url.indexOf('/node_modules') === 0) {
-        fs.readFile(__dirname + req.url, function (err, data) {
-            if (err) {
-                throw err;
-            } else {
-                res.end(data);
-            }
-        });
-    } else {
-        res.end('请求路径： ' + req.url);
-    }
-});
-
-//4.监听端口号
-app.listen(3000, function () {
-    console.log('欢迎来到王者荣耀英雄管理器');
-});*/
 //1.导入http模块
 var http = require('http');
 //导入文件模块
@@ -86,11 +8,6 @@ var path = require('path');
 var url = require('url');
 var querystring = require("querystring");
 
-/*var postHTML =
-    '<html><head><meta charset="utf-8"></head>' +
-    '<body>' +'<h1>{{**name**}}</h1>'+
-    '</body></html>';
-var flag ;*/
 var html;
 
 http.createServer(function (request, response) {
@@ -111,17 +28,27 @@ http.createServer(function (request, response) {
     // 处理接口
     switch (request.method) {
         case "GET":
+            console.log("aaa");
             if (pathname === "/get"){
+                // console.log(name);
                 name = request.query.name;
                 response.writeHead(200, {'Content-Type': 'text/plain'});
+                // console.log(name);
                 response.end(name);
             }else if(pathname === "/post"){
                 response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
                 // response.write(postHTML);
                 response.end(html);
+            }else if(pathname === "/index/submit"){
+                response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+                fs.readFile('./index/submit.html', 'utf-8', function (err,data) {
+                    console.log(data);
+                    response.end(data);
+                });
             }
             break;
         case "POST":
+            console.log("bbb");
             if (pathname === "/post"){
                 var postData = "";
                 // 数据块接收中
@@ -133,21 +60,21 @@ http.createServer(function (request, response) {
                     console.log('数据接收完毕');
                     console.log(request.headers["content-type"]);
                     // console.log(request);
-/*
-                    if(request.headers["content-type"] === "application/x-www-form-urlencoded"){
-                        console.log("aaa");
-                        var params = querystring.parse(postData);//GET & POST  ////解释表单数据部分{name="zzl",email="zzl@sina.com"}
-                        console.log(params);
-                        console.log(params["name"]);
-                        name = params["name"]
-                    }else if(request.headers["content-type"] === "application/json"){
-                        console.log("bbb");
-                        var params = JSON.parse(postData);//GET & POST  ////解释表单数据部分{name="zzl",email="zzl@sina.com"}
-                        console.log(params);
-                        console.log(params["name"]);
-                        name = params["name"]
-                    }
-*/
+                    /*
+                                        if(request.headers["content-type"] === "application/x-www-form-urlencoded"){
+                                            console.log("aaa");
+                                            var params = querystring.parse(postData);//GET & POST  ////解释表单数据部分{name="zzl",email="zzl@sina.com"}
+                                            console.log(params);
+                                            console.log(params["name"]);
+                                            name = params["name"]
+                                        }else if(request.headers["content-type"] === "application/json"){
+                                            console.log("bbb");
+                                            var params = JSON.parse(postData);//GET & POST  ////解释表单数据部分{name="zzl",email="zzl@sina.com"}
+                                            console.log(params);
+                                            console.log(params["name"]);
+                                            name = params["name"]
+                                        }
+                    */
 
                     postData= querystring.parse(postData);
                     // response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});//设置response编码为utf-8
@@ -155,8 +82,6 @@ http.createServer(function (request, response) {
                     // response.write(util.inspect(params));
                     // response.end("数据提交完毕");
                     // response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-                    response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-
 
                     if(postData.name) { // 输出提交的数据
                         fs.readFile('./index/template.html', 'utf-8', function (err,data) {
@@ -209,5 +134,5 @@ http.createServer(function (request, response) {
     // console.log(request.url);
     // console.log(request.method);
 
-}).listen(8888);
-console.log('Server running at http://127.0.0.1:8888/');
+}).listen(8001);
+console.log('Server running at http://zzj19980514.xyz:8001/');
